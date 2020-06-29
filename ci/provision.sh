292df49b063e76213b6bc7cb209144b8243ab317
Johan Siebens <johan.siebens@gmail.com>
2020-06-29 08:03:18 +0200

    add vagrant setup and makefile
    Signed-off-by: Johan Siebens <johan.siebens@gmail.com>

--- ci/provision.sh
+++ ci/provision.sh
@@ -6,20 +6,18 @@ sudo apt-get install -y \
     qemu-user-static \
     wget \
     curl \
+    zip \
     unzip \
     xz-utils
 
 # Download and install packer
 [[ -e /tmp/packer ]] && rm -rf /tmp/packer*
-wget https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip \
-    -q -O /tmp/packer_1.6.0_linux_amd64.zip
+wget https://releases.hashicorp.com/packer/1.6.0/packer_1.6.0_linux_amd64.zip -q -O /tmp/packer_1.6.0_linux_amd64.zip
 pushd /tmp
 unzip -u packer_1.6.0_linux_amd64.zip
 sudo cp packer /usr/local/bin
 rm -rf /tmp/packer*
 popd
 
-pushd /usr/local/bin
-sudo wget https://github.com/solo-io/packer-builder-arm-image/releases/download/v0.1.6/packer-builder-arm-image
-sudo chmod 755 packer-builder-arm-image
-popd
+sudo wget https://github.com/solo-io/packer-builder-arm-image/releases/download/v0.1.6/packer-builder-arm-image -q -O /usr/local/bin/packer-builder-arm-image
+sudo chmod 755 /usr/local/bin/packer-builder-arm-image
